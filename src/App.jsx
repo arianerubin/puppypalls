55824c71bbb003d9dd73d96b3882168fbb2daefa
arianerubin <165211589+arianerubin@users.noreply.github.com>
2024-06-12 14:13:40 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import { puppyList } from "./data.js";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  return <></>;
+  function handleClick() {
+    //something
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div className="puppyDetails">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
